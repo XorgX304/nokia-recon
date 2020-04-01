49e2a69696ff6e579e5e0ac20e6003db447aed2f
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-04-01 21:54:08 +0200

    Create phpinfo.php

---
+++ reports/medium/phpinfo.php
@@ -0,0 +1,10 @@
+## how to get this file
+
+im using this tool for fetch all paths <a href='https://github.com/knassar702/opener'>Open3r</a>
+
+## Command
+`$ cat hosts | python3 open3r -p /phpinfo.php -n 200`
+
+### hoo hooo 
+
+<img src='src/phpinfo.png'>
